12e1d2a5705e3e2e909f117f1dcacac0b2ab3731
Saveen Reddy <saveenr@live.com>
2018-05-23 19:13:58 -0700

    Added code

---
+++ USQL_Code_Behind_Demo/USQL_Code_Behind_Demo/Script.usql.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeBehind
+{
+    public static class MyHelpers
+    {
+        public static string MyToUpper(string s)
+        {
+            return (s + s).ToUpper();
+        }
+    }
+}
+
+
+
+
